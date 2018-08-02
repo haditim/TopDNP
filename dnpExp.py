aaf368b0c144ee80cb5d60260e54422586ad1124
hadi <hhhadddi@yahoo.com>
2018-08-02 11:27:44 +0200

    First commit

---
+++ dnpExp.py
@@ -0,0 +1,262 @@
+##################################################################
+#
+# Sets up an ODNP and T1 series experiment based on user input and template experiments.
+# by M. Hadi Timachi
+#
+##################################################################
+# Adjust things here
+# To control B12
+python27Path = 'C:\\Python27\\python.exe'
+b12CodePath = 'C:\\Bruker\\TopSpin3.2\\exp\\stan\\nmr\\py\\user\\imports\\b12.py'
+b12TempFile = 'C:\\Users\\nmrsu\\Desktop\\tempTopSpin'
+b12RunTime = '30' # The time that the external code for B12 should keep running in minutes
+# IP address of gpib to etherenet converter connected to power meter
+pMeterIP = '134.147.197.144'
+# General parameters
+expsPath = 'C:\NMRUserData\HadiTimachi'
+waitTime = 4  # Time to wait after giving order to B12
+powerReadoutAverage = 50  # The number of averages made to get the real power readout
+
+import os
+import subprocess
+import datetime
+import math
+import time
+import imports.gpib_eth as g
+
+# Start code to control B12
+subprocess.Popen('%s %s %s' % (python27Path, b12CodePath, b12TempFile))
+
+
+# Functions we need :
+def dec_range_non_linear(x, y, steps, linFraction=2):
+    """
+    Calculates steps for DNP experiment. It divides the whole range with linFraction, takes the first part as linear
+    and the second part as logarithmic :param x: start power (dBm) :param y: end power (dBm) :param steps: number of
+    steps :param linFraction: the division factor :return: list of powers
+    """
+    xInc = float(x)
+    x = float(x)
+    y = float(y)
+    linearStep = float(int(steps) // linFraction)
+    logStep = steps - linearStep
+    for i in range(0, linearStep):
+        yield xInc
+        xInc += (y // linFraction - x) // linearStep
+    for i in range(linearStep, steps):
+        xInc = w_to_dbm(dbm_to_w(xInc) + float((dbm_to_w(y) - dbm_to_w(y // linFraction)) / float(logStep)))
+        yield float(xInc)
+
+
+def dec_range_linear(x, y, steps):
+    """
+    Calculates linear steps (for T1 experiments)
+    :param x: start power (dBm)
+    :param y: end power (dBm)
+    :param steps: number of steps
+    :return: list of powers
+    """
+    xInc = float(x)
+    y = float(y)
+    for i in range(0, steps):
+        yield xInc
+        xInc = w_to_dbm(dbm_to_w(xInc) + float((dbm_to_w(y) - dbm_to_w(x)) / float(steps)))
+
+
+def dbm_to_w(dbmPower):
+    return (10 ** (float(dbmPower) / 10)) / 1000.
+
+
+def w_to_dbm(wPower):
+    return (10. * math.log10(1000 * float(wPower)))
+
+
+# Powermeter connection
+def connect_to_power_meter(gpibaddr=14, ipaddr=pMeterIP):
+    powerConn = g.gigatronics_powermeter(gpibaddress=gpibaddr, ip=ipaddr)
+    return powerConn
+
+
+# Initialize C:\tempTopSpin for B12 commands
+# Unfortunately, we have to open and close the file every time we want B12 commands
+b12File = open(b12TempFile, 'r+')
+freqResult = INPUT_DIALOG("Frequency input",
+                          "Please set the waveguide switch to DNP mode\n"
+                          "(and yell at B12 for not implementing this yet)\n "
+                          "AND\n"
+                          "enter center frequency to set for B12.",
+                          ["Frequency [kHz] = "], ["9600000"], [""], ["1"])
+b12File.write("power 0 \n")
+b12File.write("freq " + freqResult[0] + "\n")
+b12File.close()
+
+# datetime for experiment folder name
+dT = datetime.date.today().strftime("%Y%m%d_CWODNP_")
+
+# test connections:
+try:
+    powerConn = connect_to_power_meter()
+except:
+    ERRMSG("Error in connecting to power meter. Is it connected?!", "Power meter error")
+    EXIT()
+
+# Ask for exp. name and folder
+expNameDia = dialogs.MultiLineInputDia("",
+                                       "Please enter parameters for the new DNP experiment:",
+                                       ["Folder path", "Folder name"],
+                                       [expsPath, dT],
+                                       ["0", "1"], ["", ""],
+                                       None, None, 0, 15, 0, None)
+expNameDia.setExitUponEnter(0)
+expNameDia.setVisible(1)
+expNameResult = expNameDia.getValues()
+expPath = os.path.join(expNameResult[0], expNameResult[1])
+if not os.path.exists(expPath):
+    os.makedirs(expPath)
+else:
+  MSG("It seems that folder %s exists. Please rename that if you want to have a new exp." %
+      str(os.path.join(expNameResult[0],expNameResult[1])))
+  EXIT()
+
+MSG("Experiment path is going to be: \n %s" % str(expPath))
+
+dia = dialogs.MultiLineInputDia("",
+                                "Please enter parameters for experiment:",
+                                ["Minimum power set for DNP [dBm] = ",
+                                 "Maximum power set for DNP [dBm] = ",
+                                 "Number of steps for DNP series = ",
+                                 "NS for DNP",
+                                 "Do T1 series [0=No, 1=Yes]",
+                                 "Minimum power set for T1 [dBm] = ",
+                                 "Maximum power set for T1 [dBm] = ",
+                                 "Number of steps for T1 series = ",
+                                 "NS for T1"],
+                                ["0", "30", "10", "3", "1", "0", "30", "4", "3"],
+                                ["1", "1", "1", "1", "1", "1", "1", "1", "1"], ["", "", "", "", "", "", "", "", ""],
+                                None, None, 0, 15, 0, None)
+dia.setExitUponEnter(0)
+dia.setVisible(1)
+result = dia.getValues()
+
+if result == None:  # Canceled by user
+    EXIT()
+else:
+    dnpMinP, dnpMaxP, dnpSteps, dnpNS, doT1, t1MinP, t1MaxP, t1Steps, t1NS = result
+
+dnpPowerRange = [round(x, 1) for x in dec_range_non_linear(dnpMinP, dnpMaxP, int(dnpSteps))]
+t1PowerRange = [round(x, 1) for x in dec_range_linear(t1MinP, t1MaxP, int(t1Steps))]
+if int(doT1) == 1:
+    if not CONFIRM("CONFIRMATION", "About to do DNP and T1 series \n" +
+                                   "DNP power range: %s" % str(dnpPowerRange) +
+                                   "\n T1 power range: %s" % str(t1PowerRange) +
+                                   "\n Is it correct?") == 1:
+        EXIT()
+    try:
+        os.makedirs(os.path.join(expPath, "1"))
+        os.makedirs(os.path.join(expPath, "50"))
+        # This is not a good idea, but Bruker runs the code in another folder
+        os.system("xcopy C:\Bruker\TopSpin3.2\exp\stan\\nmr\py\user\\1 %s /E" % os.path.join(expPath, "1"))
+        os.system("xcopy C:\Bruker\TopSpin3.2\exp\stan\\nmr\py\user\\50 %s /E" % os.path.join(expPath, "50"))
+    except:
+        # ERRMSG("Error copying folders!", "Copy error")
+        # EXIT()
+        pass
+else:
+    if not CONFIRM("CONFIRMATION", "About to do DNP\n" +
+                                   "DNP power range: %s" % str(dnpPowerRange) +
+                                   "\n Is it correct?") == 1:
+        EXIT()
+    try:
+        os.makedirs(os.path.join(expPath, "1"))
+        # This is not a good idea, but Bruker runs the code in another folder
+        os.system("xcopy C:\Bruker\TopSpin3.2\exp\stan\\nmr\py\user\\1 %s /E" % os.path.join(expPath, "1"))
+    except:
+        pass
+
+RE([str(expNameResult[1]), "1", "1", str(expNameResult[0])], "y")
+# set NS
+XCMD("NS " + dnpNS)
+# run freq adjustment
+ZG()
+EFP()
+MSG("Please adjust frequency offset THEN click close to continue.", "Frequency adjustment")
+# DNP loop
+for i, dnpSet in enumerate(dnpPowerRange):
+    curExpNo = str(i + 2)
+    prevExpNo = str(i + 1)
+    curExpPath = os.path.join(expPath, curExpNo)
+    prevExpPath = os.path.join(expPath, prevExpNo)
+    powerAvg = 0
+    # Set B12
+    b12File = open(b12TempFile, 'r+')
+    b12File.write("power %s \n" % str(int(dnpSet * 10)))
+    b12File.close()
+    # This should be optimized
+    time.sleep(waitTime)
+    for j in range(0, powerReadoutAverage):
+        powerAvg += powerConn.read_power()
+        time.sleep(.1)
+    powerAvg /= float(powerReadoutAverage)
+    # MSG("power for exp %s powerset %s is %s" % (curExpNo, str(int(dnpSet * 10)), str(powerAvg)))
+    try:
+        os.makedirs(curExpPath)
+        os.system("xcopy %s %s /E" % (prevExpPath, curExpPath))
+        if os.path.isfile(os.path.join(curExpPath, "fid")):
+            os.remove(os.path.join(curExpPath, "fid"))
+        if os.path.exists(os.path.join(curExpPath, "pdata", "999")):
+            os.system("RMDIR %s /s /q" % os.path.join(curExpPath, "pdata", "999"))
+    except:
+        ERRMSG("Error copying folders!", "Copy error")
+        EXIT()
+    try:
+        if os.path.isfile(os.path.join(curExpPath, "pdata", "1", "title")):
+            os.remove(os.path.join(curExpPath, "pdata", "1", "title"))
+        titleFile = open(os.path.join(curExpPath, "pdata", "1", "title"), 'w')
+        titleFile.write("CWODNP set %.3f dBm" % powerAvg)
+        titleFile.close()
+    except:
+        ERRMSG("Error changing title!", "Title change error")
+    RE([str(expNameResult[1]), curExpNo, "1", str(expNameResult[0])], "y")
+    # run the experiment
+    ZG()
+
+# T1 loop
+if doT1 and t1Steps>0:
+    for i, t1Set in enumerate(t1PowerRange):
+        curExpNo = str(i + 50)
+        prevExpNo = str(i + 49)
+        curExpPath = os.path.join(expPath, curExpNo)
+        prevExpPath = os.path.join(expPath, prevExpNo)
+        powerAvg = 0
+        # Set B12
+        b12File = open(b12TempFile, 'r+')
+        b12File.write("power %s \n" % str(int(dnpSet * 10)))
+        b12File.close()
+        # This should be optimized
+        time.sleep(waitTime)
+        for j in range(0, powerReadoutAverage):
+            powerAvg += powerConn.read_power()
+            time.sleep(.1)
+        powerAvg /= float(powerReadoutAverage)
+        # MSG("power for exp %s powerset %s is %s" % (curExpNo, str(int(dnpSet * 10)), str(powerAvg)))
+        if i != 0:
+            try:
+                os.makedirs(curExpPath)
+                os.system("xcopy %s %s /E" % (prevExpPath, curExpPath))
+                if os.path.isfile(os.path.join(curExpPath, "ser")):
+                    os.remove(os.path.join(curExpPath, "ser"))
+            except:
+                ERRMSG("Error copying T1 folders!", "Copy error")
+                EXIT()
+        try:
+            if os.path.isfile(os.path.join(curExpPath, "pdata", "1", "title")):
+                os.remove(os.path.join(curExpPath, "pdata", "1", "title"))
+            titleFile = open(os.path.join(curExpPath, "pdata", "1", "title"), 'w')
+            titleFile.write("T1 set %.3f dBm" % powerAvg)
+            titleFile.close()
+        except:
+            ERRMSG("Error changing T1 exp. title!", "Title change error")
+        RE([str(expNameResult[1]), curExpNo, "1", str(expNameResult[0])], "y")
+        # run the experiment
+        ZG()
+MSG("DNP exp. done", "Done")
---
+++ imports/b12.py
@@ -0,0 +1,78 @@
+##################################################################
+#
+# Talks to Bridge12 MPS with commands received in tempFile file.
+# A part of TopDNP
+# by M. Hadi Timachi
+#
+##################################################################
+
+
+import time
+import serial
+import serial.tools.list_ports
+import sys
+
+try:
+    tempFile = sys.argv[1]
+    runTime = sys.argv[2]
+except Exception as e:
+    print "Error {} occured when trying to get tempFile and runTime from argv".format(e)
+    tempFile = "C:\\tempTopSpin"
+    runTime = 30
+
+
+class Bridge12():
+    def __init__(self, baudRate=115200, timeout=1):
+        arduino_ports = [
+            p.device
+            for p in serial.tools.list_ports.comports()
+            if 'Arduino' in p.description
+        ]
+        if not arduino_ports:
+            raise IOError("No Arduino found")
+        if len(arduino_ports) > 1:
+            warnings.warn('Multiple Arduinos found - using the first')
+        self.con = serial.Serial(arduino_ports[0], baudrate=baudRate, timeout=timeout)
+        if not "MPS" in self.read():
+            raise IOError("B12 does not reply")
+        else:
+            print "Successfully connected to B12"
+
+    def send_command(self, cmd):
+        self.con.write(cmd + "\n")
+        return "Me: %s" % cmd
+
+    def read(self):
+        value = self.con.readline().decode('ascii')
+        return "B12: %s" % value
+
+    def close(self):
+        return self.con.close()
+
+
+print sys.version
+try:
+    bridge12_con = Bridge12()
+    print "B12 connected. Ready for commands."
+except Exception as e:
+    print 'Error {} in connecting to B12.'.format(e)
+
+commands = []
+temp = tempFile
+i = 0
+while i < runTime*60:
+    i += 1
+    with open(temp, 'r') as tempFile:
+        lines = tempFile.readlines()
+        for line in lines:
+            commands.append(line)
+    with open(temp, 'w') as delFile:
+        delFile.write("")
+    for i, command in enumerate(commands):
+        print "command received %s" % command
+        bridge12_con.send_command(command)
+        print bridge12_con.read()
+        commands.pop(i)
+    if i % 15 == 0:
+        print "I'm waiting for commands in file {}".format(tempFile)
+    time.sleep(1)
